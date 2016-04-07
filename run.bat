17e54519a06b16fe7c789f69566e9e57676ca9a3
Patrick Snape <patricksnape@gmail.com>
2016-04-07 08:26:07 +0100

    Wrap platform in quotes - add test for different VS 2008

--- run.bat
+++ run.bat
@@ -1,8 +1,7 @@
-echo %PLATFORM%
-if %PLATFORM% == "x86" (
+if "%PLATFORM%" == "x86" (
     set MINICONDA_DIR=C:\Miniconda\Scripts
 )
-if %PLATFORM% == "x64" (
+if "%PLATFORM%" == "x64" (
     set MINICONDA_DIR=C:\Miniconda-x64\Scripts
 )
 
@@ -11,5 +10,16 @@ if %PLATFORM% == "x64" (
 %MINICONDA_DIR%\conda.exe install conda-build -y
 
 %MINICONDA_DIR%\pip.exe install https://github.com/patricksnape/conda-build/zipball/vs_logic --upgrade
- 
-%MINICONDA_DIR%\conda.exe build recipe --py=%PYTHON_VERSION%
+
+if "%PYTHON_VERSION%" == "2.7" (
+    rem Test Python tools for VS
+    %MINICONDA_DIR%\conda.exe build recipe --py=%PYTHON_VERSION%
+    if errorlevel 1 exit 1
+    rem Test VS 2008 - First remove vcvarsall for Python tools
+    rm %LOCALAPPDATA%\Programs\Common\Microsoft\Visual C++ for Python\9.0\vcvarsall.bat
+    %MINICONDA_DIR%\conda.exe build recipe --py=%PYTHON_VERSION%
+    if errorlevel 1 exit 1
+) else (
+    %MINICONDA_DIR%\conda.exe build recipe --py=%PYTHON_VERSION%
+    if errorlevel 1 exit 1
+)
