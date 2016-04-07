945015050c9de9f0da5d78aee789105636a81675
Patrick Snape <patricksnape@gmail.com>
2016-04-07 08:12:09 +0100

    Add basic CMake project
    Still need to update building to test different compilers

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,1 +1,31 @@
-copy %RECIPE_DIR% . /E /NFL /NDL
+conda build --version
+
+robocopy %RECIPE_DIR%\project . /E /NFL /NDL
+
+mkdir build
+cd build
+
+set CMAKE_CONFIG="Release"
+
+if "%PY_VER%" == "3.4" (
+    set GENERATOR=Visual Studio 10 2010
+) else (
+    if "%PY_VER%" == "3.5" (
+        set GENERATOR=Visual Studio 14 2015
+    ) else (
+        set GENERATOR=Visual Studio 9 2008
+    )
+)
+
+if %ARCH% EQU 64 (
+    set GENERATOR=%GENERATOR% Win64
+)
+
+cmake .. -G"%GENERATOR%" -DCMAKE_BUILD_TYPE=%CMAKE_CONFIG%
+
+cmake --build . --config %CMAKE_CONFIG% --target ALL_BUILD
+cmake --build . --config %CMAKE_CONFIG% --target INSTALL
+
+copy ..\bin\%CMAKE_CONFIG%\conda_test.exe %LIBRARY_BIN%\conda_test.exe
+
+if errorlevel 1 exit 1
---
+++ run.bat
@@ -0,0 +1,14 @@
+if %PLATFORM% == "x86" (
+    set MINICONDA_DIR=C:\Miniconda\Scripts
+)
+if %PLATFORM% == "x64" (
+    set MINICONDA_DIR=C:\Miniconda-x64\Scripts
+)
+
+%MINICONDA_DIR%\conda.exe update
+%MINICONDA_DIR%\conda.exe install pip -y
+%MINICONDA_DIR%\conda.exe install conda-build -y
+
+%MINICONDA_DIR%\pip.exe install https://github.com/patricksnape/conda-build/zipball/vs_logic --upgrade
+ 
+%MINICONDA_DIR%\conda.exe build recipe --py=%PYTHON_VERSION%
