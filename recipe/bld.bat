77f9482ccf6bcc2c34e5693072fc5037644232e2
Patrick Snape <patricksnape@gmail.com>
2016-04-07 08:45:08 +0100

    Use magic VS commands

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,27 +3,32 @@ robocopy %RECIPE_DIR%\project . /E /NFL /NDL
 mkdir build
 cd build
 
-set CMAKE_CONFIG="Release"
+REM write a temporary batch file to map cl.exe version to visual studio version
+echo @echo 15=9 2008> msvc_versions.bat
+echo @echo 16=10 2010>> msvc_versions.bat
+echo @echo 19=14 2015>> msvc_versions.bat
 
-if "%PY_VER%" == "3.4" (
-    set GENERATOR=Visual Studio 10 2010
-) else (
-    if "%PY_VER%" == "3.5" (
-        set GENERATOR=Visual Studio 14 2015
-    ) else (
-        set GENERATOR=Visual Studio 9 2008
-    )
+REM Run cl.exe to find which version our compiler is
+for /f "delims=" %%A in ('cl /? 2^>^&1 ^| findstr /C:"Version"') do set "CL_TEXT=%%A"
+FOR /F "tokens=1,2 delims==" %%i IN ('msvc_versions.bat') DO echo %CL_TEXT% | findstr /C:"Version %%i" > nul && set VSTRING=%%j && goto FOUND
+EXIT 1
+:FOUND
+
+if "%ARCH%" == "64" (
+   set "VSTRING=%VSTRING%Win64"
 )
 
-if %ARCH% EQU 64 (
-    set GENERATOR=%GENERATOR% Win64
-)
+REM Trim trailing whitespace that may prevent CMake from finding which generator to use
+call :TRIM VSTRING %VSTRING%
 
-cmake .. -G"%GENERATOR%" -DCMAKE_BUILD_TYPE=%CMAKE_CONFIG%
+cmake -G "Visual Studio %VSTRING%" -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . --target INSTALL --config Release
 
-cmake --build . --config %CMAKE_CONFIG% --target ALL_BUILD
-cmake --build . --config %CMAKE_CONFIG% --target INSTALL
-
-copy ..\bin\%CMAKE_CONFIG%\conda_test.exe %LIBRARY_BIN%\conda_test.exe
-
+copy ..\bin\Release\conda_test.exe %LIBRARY_BIN%\conda_test.exe
 if errorlevel 1 exit 1
+
+:TRIM
+  SetLocal EnableDelayedExpansion
+  set Params=%*
+  for /f "tokens=1*" %%a in ("!Params!") do EndLocal & set %1=%%b
+  exit /B
