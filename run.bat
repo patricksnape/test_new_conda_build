97fb004521fcbc7db55aad41484c514f77d15850
Patrick Snape <patricksnape@gmail.com>
2016-04-23 15:37:06 +0100

    Add xonsh to see it fail

--- run.bat
+++ run.bat
@@ -10,7 +10,7 @@ if "%PLATFORM%" == "x64" (
 %MINICONDA_DIR%\conda.exe install conda-build -y
 
 %MINICONDA_DIR%\conda.exe build --version
-%MINICONDA_DIR%\pip.exe install https://github.com/patricksnape/conda-build/zipball/vs_logic --upgrade
+%MINICONDA_DIR%\pip.exe install https://github.com/patricksnape/conda-build/zipball/python34_windows_fix --upgrade
 %MINICONDA_DIR%\conda.exe build --version
 
 if "%PYTHON_VERSION%" == "2.7" (
@@ -23,5 +23,8 @@ if "%PYTHON_VERSION%" == "2.7" (
     if errorlevel 1 exit 1
 ) else (
     %MINICONDA_DIR%\conda.exe build recipe --py=%PYTHON_VERSION%
+    if errorlevel 1 exit 1
+    
+    %MINICONDA_DIR%\conda.exe build recipe_setuptools --py=%PYTHON_VERSION%
     if errorlevel 1 exit 1
 )
