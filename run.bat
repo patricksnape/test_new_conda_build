d15a595723dce33424b60735eb29ab6d66655778
Patrick Snape <patricksnape@gmail.com>
2016-05-16 20:29:38 +0100

    Use new branch - skip bespoke 2.7 logic

--- run.bat
+++ run.bat
@@ -11,21 +11,11 @@ if "%PLATFORM%" == "x64" (
 %MINICONDA_DIR%\conda.exe install conda-build -y
 
 %MINICONDA_DIR%\conda.exe build --version
-%MINICONDA_DIR%\pip.exe install https://github.com/patricksnape/conda-build/zipball/python34_windows_fix --upgrade
+%MINICONDA_DIR%\pip.exe install https://github.com/patricksnape/conda-build/zipball/more_vs_fixes --upgrade
 %MINICONDA_DIR%\conda.exe build --version
 
-if "%PYTHON_VERSION%" == "2.7" (
-    rem Test Python tools for VS
-    %MINICONDA_DIR%\conda.exe build recipe --py=%PYTHON_VERSION%
-    if errorlevel 1 exit 1
-    rem Test VS 2008 - First remove vcvarsall for Python tools
-    del "%LOCALAPPDATA%\Programs\Common\Microsoft\Visual C++ for Python\9.0\vcvarsall.bat"
-    %MINICONDA_DIR%\conda.exe build recipe --py=%PYTHON_VERSION%
-    if errorlevel 1 exit 1
-) else (
-    %MINICONDA_DIR%\conda.exe build recipe --py=%PYTHON_VERSION%
-    if errorlevel 1 exit 1
-    
-    %MINICONDA_DIR%\conda.exe build recipe_setuptools --py=%PYTHON_VERSION%
-    if errorlevel 1 exit 1
-)
+%MINICONDA_DIR%\conda.exe build recipe --py=%PYTHON_VERSION%
+if errorlevel 1 exit 1
+
+%MINICONDA_DIR%\conda.exe build recipe_setuptools --py=%PYTHON_VERSION%
+if errorlevel 1 exit 1
